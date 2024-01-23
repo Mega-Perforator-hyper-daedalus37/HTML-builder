87b02fb8af80e846bd63e11d2daa23eb3ec707bb
Jumpad <bogdanmasnij67@gmail.com>
2024-01-23 02:29:23 +0200

    fix: improve solution to the first task, but honestly, do not understand purpose of node:path, it works even without it

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,9 @@
 const fs = require('node:fs');
+const path = require('node:path');
 
-fs.createReadStream(__dirname + '/text.txt', 'utf8').on('data', (text) => {
-  console.log(text);
-});
+fs.createReadStream(path.join(__dirname + '/text.txt'), 'utf8').on(
+  'data',
+  (text) => {
+    console.log(text);
+  },
+);
